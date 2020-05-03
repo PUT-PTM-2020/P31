863bb244e3221b6aef8e850e9bf357687482bf8d
Dzemoro <dellorpl@gmail.com>
2020-05-03 20:07:32 +0200

    Point collision and dokumentation

---
+++ Gra_v0.1/Inc/PointColission.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "Includes.hpp"
+#include "Level.hpp"
+#include <utility>
+
+/**
+ * Checks if c is in <a,b> or <b,a>
+ * 
+ * ARG:
+ *  a - first number of including interval
+ *  b - second number of including interval
+ *  c - number to be checked
+ * RET:
+ *  true if c is in the interval; false otherwise
+*/
+bool lineCollision(uint8_t a, uint8_t b, uint8_t c);
+
+/**
+ * Checks if entity geometrically contains point (x,y) based on entity's coordinates, hight and width
+ * 
+ * ARG:
+ *  entity - entity for checking
+ *  point - ordered pair of 2 coordinates, x as first, y as second 
+ * RET:
+ *  true if entity contains point; false otherwise
+*/
+bool pointCollision(Entity& entity, std::pair<uint8_t, uint8_t> point);
--- Gra_v0.1/Src/Shot.cpp
+++ Gra_v0.1/Src/Shot.cpp
@@ -1,4 +1,6 @@
 #include "..\Inc\Shot.hpp"
+#include "..\Inc\Level.hpp"
+#include "..\Inc\PointColission.hpp"
 
 Shot::Shot(){}
 Shot::Shot(uint8_t X, uint8_t Y, uint8_t speed){
@@ -12,8 +14,31 @@ void Shot::setX(uint8_t X){
 void Shot::setY(uint8_t Y){
     this->positionY = Y;
 }
-void Shot::movement(){}
+void Shot::setSpeed(uint8_t speed){
+    this->speed = speed;
+}
+void Shot::movement(){
+    this->positionY += speed;
+}
 
-Entity shooted(){
-
+Entity* Shot::shooted(Level& activeLevel){
+    std::pair<uint8_t, uint8_t> point(this->positionX, this->positionY);
+    for (int i=0; i<activeLevel.Enemies.size(); i++){
+        if (pointCollision(activeLevel.Enemies.at(i), point)){
+            return & activeLevel.Enemies.at(i);
+        }
+    }
+    for (int i=0; i<activeLevel.Constructions.size(); i++){
+        if (pointCollision(activeLevel.Constructions.at(i), point)){
+            return & activeLevel.Constructions.at(i);
+        }
+    }
+    if (pointCollision(activeLevel.player, point)){
+        return & activeLevel.player;
+    }
+    else
+    {
+        return nullptr;
+    }
+    
 }
